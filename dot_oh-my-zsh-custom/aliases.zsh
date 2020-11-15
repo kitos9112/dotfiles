0f41f1f1d10c5e08c0c322a1622d1de11696f96e
Marcos Soutullo Rodriguez <marcos.soutullo@vodafone.com>
2020-11-15 12:44:31 +0000

    Add aliases and env variables

--- dot_oh-my-zsh-custom/aliases.zsh
+++ dot_oh-my-zsh-custom/aliases.zsh
@@ -1,2 +1,6 @@
 # Here lives all the aliases useful for me!
 alias brewup="brew update && brew upgrade && brew upgrade --cask && brew cleanup -v -s && brew doctor"
+
+# AWS
+alias aws-get-instanceid='export AWS_EC2_INSTANCE_ID=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${AWS_EC2_INSTANCE_NAME}" --output text --query "Reservations[*].Instances[*].InstanceId") && echo ${AWS_EC2_INSTANCE_ID}'
+# BREW
--- dot_oh-my-zsh-custom/env.zsh
+++ dot_oh-my-zsh-custom/env.zsh
@@ -18,6 +18,7 @@ export GPG_TTY=$TTY
 
 # Other alterations to PATH
 export PATH=$HOME/.local/bin:$HOME/bin:$HOME/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:$PATH
+export PATH=${TG_ENV_DIR}/bin:${TF_ENV_DIR}/bin:${PATH}
 
 # Pyenv-related envs
 eval "$(pyenv init -)"
