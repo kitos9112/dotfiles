ad36395ff3650fb69e92dfcdfa8e1389f9921c6b
Marcos Soutullo <marcos.soutullo91@gmail.com>
2020-12-12 21:05:29 +0000

    Minor improvements

--- dot_oh-my-zsh-custom/env.zsh
+++ dot_oh-my-zsh-custom/env.zsh
@@ -21,13 +21,17 @@ export LANG=en_GB.UTF-8
 # Fix gpg: signing failed: Inappropiate ioctl for device
 export GPG_TTY=$TTY
 
+#aws --version | grep -q "aws-cli/2" && source /usr/local/bin/aws_zsh_completer.sh
+# Kubectl autocomplete
+if [ $commands[kubectl] ]; then source <(kubectl completion zsh); fi
+
 # Other alterations to PATH
-export PATH=${HOME}/.local/bin:${HOME}/bin:/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:${PATH}
-export PATH=${HOME}/.tfenv/bin:${HOME}/.tgenv/bin:${PATH}
+export PATH=${PYENV_ROOT}:${HOME}/.local/bin:${HOME}/bin:/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:${PATH}
+export PATH=${HOME}/.tfenv/bin:${HOME}/.tgenv/bin:$HOME/.poetry/bin:${PATH}
 
 # Pyenv-related envs
 eval "$(pyenv init -)"
-
+eval "$(pyenv virtualenv-init -)"
 if $(uname -a | grep -qPe "(M|m)icrosoft"); then
   # Hitting a WSL system :)
   export DISPLAY="$(/sbin/ip route | awk '/default/ { print $3 }'):0"
