fa5474dad5879c36a22bde3a6e26e9a49ecb4545
Marcos Soutullo <marcos.soutullo91@gmail.com>
2021-01-02 17:22:45 +0100

    Add a few handy SH functions

--- dot_oh-my-zsh-custom/functions.zsh
+++ dot_oh-my-zsh-custom/functions.zsh
@@ -1,5 +1,5 @@
-mkpoetryproj ()
-{
+# Create a Poetry Python directory within VSCODE
+mkpoetryproj () {
     if [ $# -eq 1 ]; then
         poetry new "$1"
         cd "$1" || exit
@@ -40,4 +40,81 @@ mkpoetryproj ()
         echo "and adds black and mypy, and makes sure VSCode knows about them"
         echo "it then inits a git repo, adds everything and commits it, then opens VSCode"
     fi
+}
+
+# Determine size of a file or total size of a directory
+fs() {
+	if du -b /dev/null > /dev/null 2>&1; then
+		local arg=-sbh
+	else
+		local arg=-sh
+	fi
+	# shellcheck disable=SC2199
+	if [[ -n "$@" ]]; then
+		du $arg -- "$@"
+	else
+		du $arg -- .[^.]* *
+	fi
+}
+
+# Create a .tar.gz archive, using `zopfli`, `pigz` or `gzip` for compression
+targz() {
+	local tmpFile="${1%/}.tar"
+	tar -cvf "${tmpFile}" --exclude=".DS_Store" "${1}" || return 1
+
+	size=$(
+	stat -f"%z" "${tmpFile}" 2> /dev/null; # OS X `stat`
+	stat -c"%s" "${tmpFile}" 2> /dev/null # GNU `stat`
+	)
+
+	local cmd=""
+	if (( size < 52428800 )) && hash zopfli 2> /dev/null; then
+		# the .tar file is smaller than 50 MB and Zopfli is available; use it
+		cmd="zopfli"
+	else
+		if hash pigz 2> /dev/null; then
+			cmd="pigz"
+		else
+			cmd="gzip"
+		fi
+	fi
+
+	echo "Compressing .tar using \`${cmd}\`…"
+	"${cmd}" -v "${tmpFile}" || return 1
+	[ -f "${tmpFile}" ] && rm "${tmpFile}"
+	echo "${tmpFile}.gz created successfully."
+}
+
+# Create a git.io short URL
+gitio() {
+	if [ -z "${1}" ] || [ -z "${2}" ]; then
+		echo "Usage: \`gitio slug url\`"
+		return 1
+	fi
+	curl -i https://git.io/ -F "url=${2}" -F "code=${1}"
+}
+
+# Start an HTTP server from a directory, optionally specifying the port
+server() {
+	local port="${1:-8000}"
+	sleep 1 && open "http://localhost:${port}/" &
+	# Set the default Content-Type to `text/plain` instead of `application/octet-stream`
+	# And serve everything as UTF-8 (although not technically correct, this doesn’t break anything for binary files)
+	python -c $'import SimpleHTTPServer;\nmap = SimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map;\nmap[""] = "text/plain";\nfor key, value in map.items():\n\tmap[key] = value + ";charset=UTF-8";\nSimpleHTTPServer.test();' "$port"
+}
+
+restart_gpgagent(){
+	# Restart the gpg agent.
+	echo "Restarting gpg-agent and scdaemon..."
+	echo -e "\tgpg-agent: $(pgrep gpg-agent) | scdaemon: $(pgrep scdaemon)"
+
+	echo "Killing processes..."
+	# shellcheck disable=SC2046
+	kill -9 $(pgrep scdaemon) $(pgrep gpg-agent) >/dev/null 2>&1
+	echo -e "\tgpg-agent: $(pgrep gpg-agent) | scdaemon: $(pgrep scdaemon)"
+
+	gpgconf --reload gpg-agent
+	gpg-connect-agent updatestartuptty /bye >/dev/null 2>&1
+
+	echo "Restarted gpg-agent and scdaemon..."
 }
