df9d5c99666941d78d6167b33904c5cffdcebfff
Marcos Soutullo Rodriguez <marcos.soutullo91@gmail.com>
2022-04-16 00:01:07 +0100

    feat: layout improvements and minor fixes

--- dot_oh-my-zsh-custom/functions.zsh
+++ dot_oh-my-zsh-custom/functions.zsh
@@ -108,7 +108,9 @@ gitio() {
 }
 
 # Populates a local .gitignore file with the contents of a remote one
-function gi() { curl -sL https://www.toptal.com/developers/gitignore/api/$@; }
+function gi() {
+	curl -sL https://www.toptal.com/developers/gitignore/api/$@
+}
 
 _gitignoreio_get_command_list() {
 	curl -sL https://www.toptal.com/developers/gitignore/api/list | tr "," "\n"
@@ -125,7 +127,7 @@ server() {
 	local port="${1:-8000}"
 	sleep 1 && open "http://localhost:${port}/" &
 	# Set the default Content-Type to `text/plain` instead of `application/octet-stream`
-	# And serve everything as UTF-8 (although not technically correct, this doesn’t break anything for binary files)
+	# And serve everything as UTF-8 (although not technically correct, this doesn't break anything for binary files)
 	python -c $'import SimpleHTTPServer;\nmap = SimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map;\nmap[""] = "text/plain";\nfor key, value in map.items():\n\tmap[key] = value + ";charset=UTF-8";\nSimpleHTTPServer.test();' "$port"
 }
 
