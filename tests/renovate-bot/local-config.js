1891ec861a72f15b3fd878364027fa8d5b6c9a9f
Marcos Soutullo Rodriguez <marcos.soutullo91@gmail.com>
2022-08-27 17:17:58 +0100

    feat: re-adjust renovatebot and ASDF tool-version regexManager
    fix: regexManager for tools-version file using renovatebot
    
    chore: get rid of versioning for regexManager tool-version
    
    fix: add extract version template
    
    fix: add extraVersion for capturing release name
    
    fix: regex expression
    
    chore: re-attempt it
    
    fix: delete version extraction
    
    fix: do not add depName
    
    fix: test with leading v
    
    fix: remove current value with dedicated regex
    
    chore: keep trying regex
    
    fix: disable aquaproj renovate bot extensions
    
    chore: another try
    
    fix: add master branch to renovate trigger
    
    fix: another try
    
    fix: remove dataSource template
    
    chore(deps): update dependency helm to v3.9.4 [ci-skip]
    
    chore(deps): update dependency kubectl to v1.23.10 [ci-skip]
    
    chore(deps): update dependency terragrunt to v0.38.8 [ci-skip]
    
    chore(deps): update dependency kubectl to v1.25.0 [ci-skip]
    
    fix: re-adjust tool-versions file
    
    chore(deps): update dependency pluto to v5.10.6 [ci-skip]
    
    chore(deps): update dependency terraform to v1.2.8 [ci-skip]
    
    fix: do not merge minor automatically
    
    fix: adjust tool-version to accomodate current demands

---
+++ tests/renovate-bot/local-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  platform: 'github',
+  repositories: ['kitos9112/dotfiles'],
+  includeForks: true,
+  onboarding: false,
+  gitAuthor: 'Self-hosted Renovate Bot <xyz@gmail.com>',
+};
