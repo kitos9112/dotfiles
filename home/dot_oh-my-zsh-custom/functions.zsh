aa7c69f242434aa080603d4a484058ad9aa86eeb
Marcos Soutullo Rodriguez <marcos.soutullo91@gmail.com>
2023-03-19 20:47:13 +0000

    chore: add extra SH functions

--- home/dot_oh-my-zsh-custom/functions.zsh
+++ home/dot_oh-my-zsh-custom/functions.zsh
@@ -196,3 +196,23 @@ deduplicate_env_path() {
 
 # Remove duplicated PATH entries
 deduplicate_env_path
+
+# Iterates over all lines of the default ~/.tool-versions file and removes all versions that are no longer needed
+asdf-versions-cleanup() {
+	while read -r line; do
+		IFS=' ' read -r -a tool_and_versions <<< "$line"
+		# Split out the tool name and versions
+		tool_name="${tool_and_versions[0]}"
+		global_versions=("${tool_and_versions[@]:1}")
+
+		# Loop over each version of the tool name
+		for version in $(asdf list $tool_name); do
+			# When version not in `global_versions` array from .tool-versions file
+			if [[ ! " ${global_versions[@]} " =~ " ${version} " ]]; then
+				# Remove the version here if you want
+				echo "$tool_name version $version not found in .tool-versions"
+				asdf uninstall $tool_name $version
+			fi
+		done
+	done < ~/.tool-versions
+}
