620c3569976ab4c112546002f20c489dc90031eb
Marcos Soutullo Rodriguez <marcos.soutullo91@gmail.com>
2023-11-16 20:56:25 +0000

    feat: Add support to auto-integration with 1Password CLI
    * Housekeeping on homebrew by removing 1password-cli brew formula
    * Add pre-commit to homebrew formula on Linux admin installs

---
+++ home/executable_dot_op-ssh-agent.sh
@@ -0,0 +1,16 @@
+# Configure ssh forwarding
+export SSH_AUTH_SOCK=$HOME/.1password/agent.sock
+
+op.exe signin
+
+if ! pgrep -f "/npiperelay.exe" >/dev/null; then
+    if [[ -S $SSH_AUTH_SOCK ]]; then
+        # not expecting the socket to exist as the forwarding command isn't running (http://www.tldp.org/LDP/abs/html/fto.html)
+        echo "Removing previous Unix domain socket $SSH_AUTH_SOCK before starting SSH-Agent relay ..."
+        rm -rf $SSH_AUTH_SOCK
+    fi
+    echo "Starting SSH-Agent relay..."
+    # setsid to force new session to keep running
+    # set socat to listen on $SSH_AUTH_SOCK and forward to npiperelay which then forwards to openssh-ssh-agent on windows
+    (setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"npiperelay.exe -ei -s //./pipe/openssh-ssh-agent",nofork &) >/dev/null 2>&1
+fi
